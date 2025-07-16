ce141bd697dcfc9a54f2d3d2f6296c2116e240c7
akashvicky1 <143933514+akashvicky1@users.noreply.github.com>
2025-07-16 14:50:53 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,13 +1,13 @@
-// js/firebase-config.js
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDZLrF220ViXqBaYV7nB8aDZEFuO51niZg",
   authDomain: "zency-creation-project.firebaseapp.com",
   projectId: "zency-creation-project",
-  storageBucket: "zency-creation-project.firebasestorage.app",
+  storageBucket: "zency-creation-project.appspot.com",
   messagingSenderId: "281015807301",
   appId: "1:281015807301:web:7330f1622afa970298622c",
   measurementId: "G-BYZ982SG4P"
 };
 
-// Initialize Firebase
+// ✅ Initialize Firebase App
 firebase.initializeApp(firebaseConfig);
